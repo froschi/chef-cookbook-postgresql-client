f9492ced7aca71b3dbd228385e21e2bd986980c5
Thorsten Fischer <thorsten@froschi.org>
2012-06-12 11:02:02 +0200

    Amended for use on lucid, precise.
    Also, added dependencies.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libedit", ">= 0.0.1"
+depends          "libpq", ">= 0.0.1"
+depends          "libreadline", ">= 0.0.1"
+depends          "libssl", ">= 0.0.1"
+depends          "postgresql-client-common", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,21 @@
+include_recipe "libpq"
+include_recipe "libssl"
+include_recipe "postgresql-client-common"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libreadline"
+
   packages |= %w/
     postgresql-client-8.4
   /
 when "precise"
+  include_recipe "libedit"
+
   packages |= %w/
-    postgresql-client-8.4
+    postgresql-client-9.1
   /
 end
 
